e77a60e631958a5dc8a4d307a5e015ec313604d6
LekzLuthor <55745807+LekzLuthor@users.noreply.github.com>
2021-10-21 03:03:08 +0500

    update_function_add

---
+++ pyqt_prj_yl/Parcing.py
@@ -0,0 +1,50 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+
+def update():
+    url = 'https://www.finanz.ru/valyuty/v-realnom-vremeni'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    data_value = soup.find('table', class_='quote_list')
+    without_headlines = data_value.find_all('td')
+
+    currencies = ['Рубль', 'Евро', 'Доллар США', 'Японская иена',
+                  'Фунт стерлингов', 'Швейцарский франк', 'Украинская гривна']
+    headlines = ['Предложение', 'Предыдущее закрытие',
+                 '%', 'Абсолютное значение', 'Время']
+
+    res = []
+    prom = []
+    for line in without_headlines:  # перевод спарсенных данных в список
+        i = line.text
+        if i.strip():
+            if i.strip() != '-':
+                prom.append(i.replace(u'\xa0', u' '))
+        else:
+            res.append(prom)
+            prom = []
+
+    res.append(prom)  # последняя строка не добавляется т.к. после неё нет пробела
+    del res[0]  # из-за того что data начинается с пустой строки в res первым добавляется пустой список.
+    del prom
+
+    with open('url_data_values.txt', 'w',
+              encoding='utf-8') as time_file:  # запись списка данных вместе с заголовками в txt файл
+        headlines_detect = 0
+        j = 7
+        val_num = 0
+        time_file.writelines(currencies[val_num] + ' ' + ' '.join(headlines) + '\n')
+        val_num += 1
+        for i in res:
+            time_file.writelines(' '.join(i) + '\n')
+            j -= 1
+            if j <= 4:
+                headlines_detect += 1
+            if headlines_detect == 4:
+                if val_num <= len(currencies) - 1:
+                    time_file.writelines(currencies[val_num] + ' ' + ' '.join(headlines) + '\n')
+                    headlines_detect = 0
+                    val_num += 1
---
+++ pyqt_prj_yl/main.py
@@ -0,0 +1,3 @@
+from Parcing import update
+
+update()
