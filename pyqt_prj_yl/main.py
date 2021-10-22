d5e0b73bdb2c9e41bd71fc1ccde36daf0784c0f3
LekzLuthor <55745807+LekzLuthor@users.noreply.github.com>
2021-10-22 16:51:12 +0500

    main_dis+part_of_main

--- pyqt_prj_yl/main.py
+++ pyqt_prj_yl/main.py
@@ -1,3 +1,28 @@
+import sys
 from Parcing import update
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
-update()
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main_window.ui', self)
+        self.CONV_WIN = ConvertWindow()
+        self.to_convert_btn.clicked.connect(self.to_convert_window)
+
+    def to_convert_window(self):
+        self.CONV_WIN.show()
+
+
+class ConvertWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('convertor_window.ui', self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec())
